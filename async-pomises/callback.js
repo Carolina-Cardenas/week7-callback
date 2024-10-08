1effffb76a229ef93777673a2d1b8810cfae5f0f
Carolina-Cardenas <carolina.beatriz.cardenas@gmail.com>
2024-10-08 20:00:29 +0200

    started the project week 8 async. await

--- async-pomises/callback.js
+++ async-pomises/callback.js
@@ -1,46 +1,102 @@
-function flipCoin (){
-    return new Promise((resolve, reject) => {
-        let result = math.random();
-        if(result >0.5) {
-           resolve("You Won!");
-         } else{
-            reject("You Lost!")
-         }
-    });
+// function flipCoin (){
+//     return new Promise((resolve, reject) => {
+//         let result = math.random();
+//         if(result >0.5) {
+//            resolve("You Won!");
+//          } else{
+//             reject("You Lost!")
+//          }
+//     });
+// }
+
+
+// flipCoin()
+// .then(message => console.log(message))
+// .catch(error => console.log(error));
+
+
+
+
+// // function fetchAdvice(){
+// // fetch('https://api.adviceslip.com/advice')
+// // .then(response => response.json())
+// // .then(data =>{
+// //     console.log("advice",data.slipe.advice)
+// // })
+// // .catch( error =>{
+// //     console.log("There was an error fetching the advice: ", error);
+// // })
+// // }
+
+// // fetchAdvice();
+
+// function flipCoinAndFetchAdvice (){
+//     flipCoin()
+//     .then(message =>{
+//         console.log(message);
+//         return fetch('https://api.adviceslip.com/advice');
+//     })
+//     .then(response => response.json())
+//     .data( =>{
+//         console.log("Advice: ", data.slip.advice);
+//     })
+//     .cath(error =>{
+//         console.log(error);
+//     });
+// }
+
+// flipCoinAndFetchAdvice();
+
+
+async function flipCoin() {
+    try {
+        let result = Math.random();
+        if (result > 0.5) {
+            return "You Won!";
+        } else {
+            throw "You Lost!";
+        }
+    } catch (error) {
+        throw error;
+    }
 }
 
-
-flipCoin()
-.then(message => console.log(message))
-.catch(error => console.log(error));
-
-
-function fetchAdvice(){
-fetch('https://api.adviceslip.com/advice')
-.then(response => response.json())
-.then(data =>{
-    console.log("advice",data.slipe.advice)
-})
-.catch( error =>{
-    console.log("There was an error fetching the advice: ", error);
-})
+async function handleFlipCoin() {
+    try {
+        const message = await flipCoin();
+        console.log(message);
+    } catch (error) {
+        console.log(error);
+    }
 }
+
+handleFlipCoin();
+
+const fetchAdvice = async () => {
+    try {
+        const response = await fetch('https://api.adviceslip.com/advice');
+        const data = await response.json();
+        console.log("Advice: ", data.slip.advice);
+    } catch (error) {
+        console.log("There was an error fetching the advice: ", error);
+    }
+};
 
 fetchAdvice();
 
-function flipCoinAndFetchAdvice (){
-    flipCoin()
-    .then(message =>{
-        console.log(message);
-        return fetch('https://api.adviceslip.com/advice');
-    })
-    .then(response => response.json())
-    .data( =>{
+const flipCoinAndFetchAdvice = async () => {
+    try {
+        const result = await flipCoin();
+        console.log(result);
+        
+        const response = await fetch('https://api.adviceslip.com/advice');
+        const data = await response.json();
         console.log("Advice: ", data.slip.advice);
-    })
-    .cath(error =>{
+        
+    } catch (error) {
+       
         console.log(error);
-    });
-}
+    }
+};
 
 flipCoinAndFetchAdvice();
---
+++ async-pomises/server/server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const { flipCoinAndFetchAdvice } = require('./callback');
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    flipCoinAndFetchAdvice();
+
+    res.end('Coin flip initiated, check console for result!');
+}).listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
